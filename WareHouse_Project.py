def9abe9eea75da62d061d24618610222dfc0c53
s57070060 <natcha.ro@hotmail.com>
2014-12-02 16:28:42 +0700

    commit-3
    ทำส่วนของโปรแกรมเสร็จเเล้ว
    hm  คือ แปลงหน่วยชม.ให้เป็นหน่วยนาที
    md คือ แปลงเดือนให้เป็นวัน
    num_column คือจำนวนเเถวที่วางสินค้า

--- WareHouse_Project.py
+++ WareHouse_Project.py
@@ -3,49 +3,73 @@ def warehouse():
     #[wide, long, high] -- input size -> float
     amount = input()
     day_deposit = raw_input().split()   # day month year(A.D)
-    time_deposit = raw_input().split()  # hour minute PM/ AM
+    time_deposit = raw_input().split()  # 00:00 - 24:00
     day_receive = raw_input().split()   
     time_receive = raw_input().split()  
-    time = {'00AM':0, '01AM':1, '02AM':2, '03AM':3, '04AM':4,\
-            '05AM':5, '06AM':6, '07AM':7, '08AM':8, '09AM':9,\
-            '11AM':11, '12AM':12, '01PM':13, '02PM':14,\
-            '03PM':15, '04PM':16, '05PM':17, '06PM':18, '07PM':19,\
-            '08PM':20, '09PM':21, '10PM':22, '11PM':23}
+    month = {'01':0, '02':31, '03':59, '04':90, '05':120, '06':151,\
+             '07':181, '08':212, '09':243, '10':273, '11':304, '12':334}
     
-    month = {'01':31, '02':29, '03':31, '04':30, '05':31, '06':30,\
-             '07':31, '08':31, '09':30, '10':31, '11':30, '12':31}
-    
-    count = 0
-    wide_t, long_t, high_t = wide*amount, longth*amount, high*amount
-    if int(wide_t) != float(wide_t):
-        wide_t = int(wide_t) + 1
-    if int(long_t) != long_t:
-        long_t = int(long_t) + 1
-    while high_t >= 10:
-       count += 1 
-       high_t -= high * int(10 / high)
-    count += 1
+    area = wide*longth
+    amount_per_column = int(10/high)
+    num_column = int(amount / amount_per_column)
+    if amount / amount_per_column != int(amount / amount_per_column):
+        num_column += 1
 
     
-    time_n, time_d, time_allday = 0, 0, 0 
     price = 0
-    if time_deposit[2] == 'PM' and time_receive[2] == 'AM'\
-       and int(day_receive[0]) - int(day_deposit[0]) >= 1:
-        if time[time_deposit[0]+time_deposit[2]] < 18:
-            price += (((60 - float(time_deposit[1]))/60) +\
-                      (17 - time[time_deposit[0]+time_deposit[2]])) * (4.5) # day
-            
-            price += (6 + time[time_receive[0]+time_receive[2]] +\
-                      float(time_receive[1])/60) * (5.5) # night
-            
-            price += (int(day_receive[0]) - int(day_deposit[0]) - 1) * 120 # all day          
-        else:
-            price += 0 # day
-            price += (23 - time[time_deposit[0]+time_deposit[2]]+\
-                      ((60 - float(time_deposit[1]))/60) +\
-                      time[time_receive[0]+time_receive[2]] +\
-                      (float(time_receive[1])/60)) * 5.5 # night
-            price += (int(day_receive[0]) - int(day_deposit[0]) - 1) * 120 # all daywarehouse()
+    hm_deposit = (int(time_deposit[0])*60) + int(time_deposit[1]) # change hour to minute
+    hm_receive = (int(time_receive[0])*60) + int(time_receive[1]) # change hour to minute
+    md_deposit = int(day_deposit[0]) + month[day_deposit[1]]
+    md_receive = int(day_receive[0]) + month[day_receive[1]]
+    # deposit and receive in 1 day
+    if day_deposit[0] == day_receive[0] and\
+       day_deposit[1] == day_receive[1] and\
+       day_deposit[2] == day_receive[2]:
+        if hm_deposit >= 0 and hm_deposit <= 360:
+            price += (360 - hm_deposit)/60.0 * 5.5
+            if hm_receive >= 0 and hm_receive <= 360:
+                price -= (360 - hm_receive)/60.0 * 5.5
+            elif hm_receive > 360 and hm_receive <= 1080:
+                price += (hm_receive - 360)/60.0 * 4.5
+            elif hm_receive >= 1080 and hm_receive <= 1440:
+                price += (720/60.0 * 4.5) + ((hm_receive - 1080)/60.0 * 5.5)
+        elif hm_deposit > 360 and hm_deposit <= 1080:
+            price += (1080 - hm_deposit)/60.0 * 4.5
+            if hm_receive > 360 and hm_receive <= 1080:
+                price -= (1080 - hm_receive)/60.0 * 4.5
+            elif hm_receive >= 1080 and hm_receive <= 1440:
+                price += (1440 - hm_receive)/60.0 * 5.5
+        elif hm_deposit > 1080 and hm_deposit <= 1440:
+            price += (1440 - hm_deposit)/60.0 * 5.5
+            if hm_receive >= 1080 and hm_receive <= 1440:
+                price -= (1440 - hm_receive)/60.0 * 5.5
 
     
+    # deposit and receive more 1 day    
+    # deposit
+    else:
+        if hm_deposit >= 0 and hm_deposit <= 360:
+            price += ((360 - hm_deposit)/60.0 * 5.5) + (12 * 4.5) + (6 * 5.5)
+        elif hm_deposit > 360 and hm_deposit <= 1080:
+            price += ((1080 - hm_deposit)/60.0 * 4.5) + (6 * 5.5)
+        elif hm_deposit > 1080 and hm_deposit <= 1440:
+            price += (1440 - hm_deposit)/60.0 * 5.5
+                            
+    # check day, month, year
+        if int(day_receive[2]) - int(day_deposit[2]) >= 1:
+            price += ((365 - md_deposit - 1) * 120) +\
+                     (int(day_receive[2]) - int(day_deposit[2] - 1)*43800) +\
+                     (md_receive * 120)
+        elif int(day_receive[2]) - int(day_deposit[2]) == 0:
+            price += (md_receive - md_deposit - 2) * 120
+    # receive
+        if hm_receive >= 0 and hm_receive <= 360:
+            price += hm_receive/60.0 * 5.5
+        elif hm_receive > 360 and hm_receive <= 1080:
+            price += (6*5.5) + ((hm_receive - 360)/60.0 * 4.5)
+        elif hm_receive > 1080 and hm_receive <= 1440:
+            price += (6*5.5) + (12*4.5) + ((hm_receive - 1080)/60.0 * 5.5)       
+    print price * num_column * area
+   
+warehouse()
     
